2fdd14d0929f4826dd0dbde55f67b4c8d39c40dc
Dxrk <118543407+dxrk09@users.noreply.github.com>
2024-09-18 19:58:12 +0100

    Add files via upload

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,29 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg"
+import { Form } from "../Form/Form";
+
+
+export function Contact(){
+
+  return(
+    <Container id="contact">
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for more information.</p>
+      </header>
+      <div className="contacts">
+        <div>
+        <a href="mailto:ilyasalla18@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:ilyasalla18@gmail.com">ilyasalla18@gmail.com</a>
+        </div>
+        <div>
+        <a href="tel:+212770420460"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+212770420460">(+212) 770420460</a>
+        </div>  
+      </div>
+      <Form></Form>
+    </Container>
+  )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,62 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+// import discordIcon from '../../assets/discord.png'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <a href="https://shadow9.it" className="logo">
+        <span>www.shadow9</span>
+        <span>IT.com</span>
+      </a>
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/ilyas-alla/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/dxrk09/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=%2B212770420460&text=Hello"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          href="https://t.me/sh9xw"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/shx9w"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
+    </Container>
+  )
+}
